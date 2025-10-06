d1facee4dc80f0d83fdff954384a6932e1897f59
robinyuan1002 <ry2361@nyu.edu>
2025-10-06 17:58:58 +0000

    update for lmbench script

--- lmbench/compile_lmbench.sh
+++ lmbench/compile_lmbench.sh
@@ -4,10 +4,10 @@ set +e
 CLANG="/home/lind/lind-wasm/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04/bin/clang"
 WASM_OPT="/home/lind/lind-wasm/tools/binaryen/bin/wasm-opt"
 WASMTIME="/home/lind/lind-wasm/src/wasmtime/target/debug/wasmtime"
-SYSROOT="/home/lind/lind-wasm/src/glibc/sysroot"
+SYSROOT="/home/lind/lind-wasm/lind-wasm-apps/lmbench/sysroot_overlay"
 
 SRC_DIR="/home/lind/lind-wasm/lind-wasm-apps/lmbench/src"
-OUT_DIR="/home/lind/lind-wasm/lind-wasm-apps/lmbench/src/wasm-bin"
+OUT_DIR="/home/lind/lind-wasm/lind-wasm-apps/lmbench/out"
 
 mkdir -p "$OUT_DIR"
 
@@ -20,9 +20,10 @@ cfiles=$(find "$SRC_DIR" -maxdepth 1 -name "*.c" \
 for cfile in $cfiles; do
     base=$(basename "$cfile" .c)
     wasm_tmp="$SRC_DIR/$base.wasm"
+    wasm_out="$OUT_DIR/$base.wasm"
     ofile="$OUT_DIR/$base.cwasm"
 
-    echo "[*] Compiling $base.c → $ofile with clang -std=gnu89"
+    echo "[*] Compiling $base.c → $wasm_out and $ofile"
 
     "$CLANG" -std=gnu89 -pthread --target=wasm32-unknown-wasi --sysroot="$SYSROOT" \
         -Wl,--import-memory,--export-memory,--max-memory=67108864,--export=__stack_pointer,--export=__stack_low \
@@ -35,6 +36,8 @@ for cfile in $cfiles; do
     fi
 
     "$WASM_OPT" --epoch-injection --asyncify -O2 --debuginfo "$wasm_tmp" -o "$wasm_tmp"
+
+    cp "$wasm_tmp" "$wasm_out"
 
     "$WASMTIME" compile "$wasm_tmp" -o "$ofile"
 
@@ -51,6 +54,7 @@ total_count=$(echo "$cfiles" | wc -l)
 echo ""
 echo "total number: $total_count"
 echo "compile error number: $fail_count"
-echo "all the compiled file in: $OUT_DIR"
+echo "all the compiled files are in: $OUT_DIR"
+
 
 
