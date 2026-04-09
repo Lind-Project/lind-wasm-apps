5021bea4515ee8337aec4984b5270793e44c35db
moogchi <seankimjr811@gmail.com>
2026-04-09 22:50:16 +0000

    Revert "modified install script to copy ca cert"
    This reverts commit aeb3197ef72aa9e186df6324ed455e5b5aed4d97.

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 LINDFS_ROOT="$1"
 APPS_BIN_DIR="$2"
 APP_NAME="$3"
-rsync -av "$APPS_BIN_DIR/$APP_NAME/" "$LINDFS_ROOT/"
 
-# Copy CA bundle for curl HTTPS support
-if [[ "$APP_NAME" == "curl" ]]; then
-    mkdir -p "$LINDFS_ROOT/etc/ssl/certs"
-    cp /etc/ssl/certs/ca-certificates.crt "$LINDFS_ROOT/etc/ssl/certs/ca-certificates.crt"
-fi
+rsync -av "$APPS_BIN_DIR/$APP_NAME/" "$LINDFS_ROOT/"
