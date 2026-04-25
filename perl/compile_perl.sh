6cfbf1ba3937b597f6057050ccefe642f329a776
Nicholas Renner <nicholassrenner@gmail.com>
2026-04-25 18:03:13 -0400

    perl: disable re extension (duplicate regcomp symbols), revert --allow-multiple-definition

--- perl/compile_perl.sh
+++ perl/compile_perl.sh
@@ -111,7 +111,7 @@ RANLIB="$RANLIB" \
   -Dranlib="$RANLIB" \
   -Doptimize="-O2 -g" \
   -Dccflags="-pthread -I$MERGED_SYSROOT/include -I$MERGED_SYSROOT/include/wasm32-wasi -Wno-incompatible-function-pointer-types" \
-  -Dldflags="-Wl,--import-memory,--export-memory,--max-memory=67108864,--export=__stack_pointer,--export=__stack_low,--export=__tls_base,--allow-multiple-definition -L$MERGED_SYSROOT/lib/wasm32-wasi -L$MERGED_SYSROOT/usr/lib/wasm32-wasi" \
+  -Dldflags="-Wl,--import-memory,--export-memory,--max-memory=67108864,--export=__stack_pointer,--export=__stack_low,--export=__tls_base -L$MERGED_SYSROOT/lib/wasm32-wasi -L$MERGED_SYSROOT/usr/lib/wasm32-wasi" \
   -Dlibs="-lpthread -lm" \
   -Dperllibs="-lpthread -lm" \
   -Uusedl \
@@ -123,6 +123,7 @@ RANLIB="$RANLIB" \
   -Ud_sigaction \
   -Ud_sigprocmask \
   -Ud_alarm \
+  --disable-mod=re \
   -Ud_crypt \
   -Ud_spawn \
   -Ud_aspawn \
